289ec2222d4e34e6ad411d956fab8965be7a1c6c
Francisco Calderon <fjavier5152@gmail.com>
2019-04-30 19:30:06 -0400

    Return thread id

--- vars/sjSlackNotifier.groovy
+++ vars/sjSlackNotifier.groovy
@@ -1,16 +1,18 @@
 #!/usr/bin/env groovy
 
-def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+def call(String buildResult, String threadId) {
+  def threadId = null
+
+  if ( buildResult == "STARTED") {
+    threadId = slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} has started", channel: threadId
+  } else if ( buildResult == "SUCCESS" ) {
+    threadId = slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful", channel: threadId
+  } else if( buildResult == "FAILURE" ) {
+    threadId = slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed", channel: threadId
+  } else if( buildResult == "UNSTABLE" ) {
+    threadId = slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable", channel: threadId
+  } else {
+    threadId = slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear", channel: threadId
   }
-  else if( buildResult == "FAILURE" ) {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
-  }
-  else if( buildResult == "UNSTABLE" ) {
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
-  }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"
-  }
+  threadId
 }
