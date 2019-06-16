d462b58fc86a06b9e2cc1b66a9caa302eb5106be
Francisco Calderon <fjavier5152@gmail.com>
2019-06-16 18:03:41 -0400

    Add helper function for optional creds

--- vars/sjBuildWebappDockerImage.groovy
+++ vars/sjBuildWebappDockerImage.groovy
@@ -25,32 +25,46 @@ def call(String environmentSufix, String environment, boolean pushToRegistry, St
                   string(credentialsId: "${environmentSufix}_send_grid_key", variable: 'send_grid_key')
                   ]) {
      script {
-         app = docker.build ("scholarjet.azurecr.io/scholarjet-webapp", '''--build-arg aws_access=$aws_access  \
-               --build-arg aws_default_bucket=$s3_bucket \
-               --build-arg aws_thumbnail_host=$aws_thumbnail_host \
-               --build-arg aws_secret=$aws_secret  \
-               --build-arg aws_transcoder_pipeline=$transcoder_pipelineId \
-               --build-arg dynamodb_table_prefix=$dynamodb_table_prefix \
-               --build-arg mailgun_access_key=$mailgun_key \
-               --build-arg mailgun_url=$mailgun_url \
-               --build-arg mailchimpApiKey=$mailchimp_api_key \
-               --build-arg mailchimpListId=$mailchimp_list_id \
-               --build-arg postToMailChimp="true" \
-               --build-arg front_end_host=$front_end_host \
-               --build-arg db_user=$db_user \
-               --build-arg db_host=$db_host \
-               --build-arg db_password=$db_password \
-               --build-arg environment=$ENV \
-               --build-arg firebase_database_url=$firebase_database_url \
-               --build-arg APPLICATION_INSIGHTS_IKEY=$app_insights_key \
-               --build-arg jwt_key=$jwt_key \
-               --build-arg sendGridKey=$send_grid_key \
-               .''')
+       send_grid_key = getStringCredential("${environmentSufix}_send_grid_key", true)
+       app = docker.build ("scholarjet.azurecr.io/scholarjet-webapp", '''--build-arg aws_access=$aws_access  \
+             --build-arg aws_default_bucket=$s3_bucket \
+             --build-arg aws_thumbnail_host=$aws_thumbnail_host \
+             --build-arg aws_secret=$aws_secret  \
+             --build-arg aws_transcoder_pipeline=$transcoder_pipelineId \
+             --build-arg dynamodb_table_prefix=$dynamodb_table_prefix \
+             --build-arg mailgun_access_key=$mailgun_key \
+             --build-arg mailgun_url=$mailgun_url \
+             --build-arg mailchimpApiKey=$mailchimp_api_key \
+             --build-arg mailchimpListId=$mailchimp_list_id \
+             --build-arg postToMailChimp="true" \
+             --build-arg front_end_host=$front_end_host \
+             --build-arg db_user=$db_user \
+             --build-arg db_host=$db_host \
+             --build-arg db_password=$db_password \
+             --build-arg environment=$ENV \
+             --build-arg firebase_database_url=$firebase_database_url \
+             --build-arg APPLICATION_INSIGHTS_IKEY=$app_insights_key \
+             --build-arg jwt_key=$jwt_key \
+             --build-arg sendGridKey=$send_grid_key \
+             .''')
          if (pushToRegistry) {
            docker.withRegistry('https://scholarjet.azurecr.io', 'azure_acr_jenkins_principal') {
                  app.push(tagName)
            }
          }
      }
+  }
+}
+
+def String getStringCredential(String credentialKey, Boolean optional) {
+  try {
+    withCredentials([string(credentialsId: credentialKey, variable: 'credentialValue')]) {
+     $credentialValue
+   }
+  } catch (Exception ex) {
+    if (!optional) {
+      throw ex
+    }
+    null
   }
 }
